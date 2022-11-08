b4d860e00da7d4baad79a54c6efe2767c0f221e8
learnitnowgit <learnitnow@onmail.com>
2022-11-07 20:41:31 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.text.html.Option;
+import java.util.Optional;
+public class Main {
+    public static void main(String[] args) {
+        Optional<Integer> optional = Optional.empty();
+        System.out.println(optional);
+        System.out.println(optional.isPresent());
+        System.out.println(optional.isEmpty());
+        System.out.println(optional.equals(Optional.empty()));
+        System.out.println("Hello world!");
+    }
+}11957185
