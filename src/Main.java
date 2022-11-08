7e720ab2f7623010adc8f395576a251c001b68c3
learnitnowgit <learnitnow@onmail.com>
2022-11-07 23:25:26 -0600

    commit with newbranch

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println(optional.isEmpty());
         System.out.println(optional.equals(Optional.empty()));
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
